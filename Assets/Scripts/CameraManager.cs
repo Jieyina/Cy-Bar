a50c114f4d61df801c88d0b3e63059e783ad89e5
Jieyina <nasjieyi@gmail.com>
2020-09-02 14:25:42 -0600

    modify camera movement

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    private float moveSpeed = 3f;
-    private float rotateSpeed = 5f;
+    public float moveSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,37 +14,7 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(new Vector3(0, 0, -moveSpeed * Time.deltaTime));
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(new Vector3(0, 0, moveSpeed * Time.deltaTime));
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Rotate(new Vector3(rotateSpeed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Rotate(new Vector3(-rotateSpeed * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
-        }
+        transform.Translate(new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0));
+        transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0));
     }
 }
