706d409cba9960b8f014f8f7c599fe41dc971f64
Jieyina <nasjieyi@gmail.com>
2020-09-09 19:29:26 -0600

    move camera with mouse

--- Assets/Scripts/Manager/CameraManager.cs
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    [SerializeField]
+    private float moveSpeed = 3f;
+    [SerializeField]
+    private int boundary = 5;
+    private Vector3 mousePos;
+    private int screenWidth;
+    private int screenHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
     }
 
     // Update is called once per frame
@@ -16,5 +24,14 @@ public class CameraManager : MonoBehaviour
     {
         transform.Translate(new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0));
         transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0));
+        mousePos = Input.mousePosition;
+        if (mousePos.x < boundary)
+            transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+        if (mousePos.x > screenWidth - boundary)
+            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        if (mousePos.y < boundary)
+            transform.Translate(new Vector3(0, -moveSpeed * Time.deltaTime, 0));
+        if (mousePos.y > screenHeight - boundary)
+            transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
     }
 }
